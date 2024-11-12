90dfcf5f8c95d5e2d94619ad54bf3d6ab50dca6d
Will Smith <williamqsmith11@gmail.com>
2024-11-12 15:39:54 -0400

    feat: make pade its own crate

--- crates/pade/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod decode;
-mod encode;
-mod header;
-mod primitives;
-// Re-export bitvec so our macro crate can rely on it
-pub use bitvec;
-pub use decode::*;
-pub use encode::*;
-pub use header::HeaderBits;
