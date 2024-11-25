4965f4ec67b9df5566e5fb2adfdbdfed7c44dfcb
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-11-25 12:27:08 -0500

    wip

--- testing-tools/src/controllers/strom/internals.rs
+++ testing-tools/src/controllers/strom/internals.rs
@@ -101,7 +101,7 @@ impl<P: WithWalletProvider> AngstromDevnetNodeInternals<P> {
             block_number,
             block_sync.clone(),
             inital_angstrom_state.pool_manager_addr,
-            false
+            true
         )
         .await;
 
