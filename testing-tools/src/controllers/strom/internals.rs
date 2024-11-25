ba49fe9c507f29f04b1d7885bafedb8015c98a96
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-11-25 12:42:08 -0500

    wip

--- testing-tools/src/controllers/strom/internals.rs
+++ testing-tools/src/controllers/strom/internals.rs
@@ -88,6 +88,7 @@ impl<P: WithWalletProvider> AngstromDevnetNodeInternals<P> {
             .get_block_number()
             .await
             .unwrap();
+        tracing::debug!(block_number, "creating strom internals");
         let block_sync = GlobalBlockSync::new(block_number);
 
         let uniswap_registry: UniswapPoolRegistry = inital_angstrom_state.pool_keys.into();
