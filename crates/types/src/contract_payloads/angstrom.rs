281ee933555a6987f99215e6ea0d6a39c65287b8
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-11-26 12:16:32 -0500

    wip

--- crates/types/src/contract_payloads/angstrom.rs
+++ crates/types/src/contract_payloads/angstrom.rs
@@ -919,8 +919,6 @@ impl AngstromPoolConfigStore {
             .await
             .map_err(|e| format!("Error getting code: {}", e))?;
 
-        println!("{code:?}");
-
         AngstromPoolConfigStore::try_from(code.as_ref())
             .map_err(|e| format!("Failed to deserialize code into AngstromPoolConfigStore: {}", e))
     }
--- testing-tools/src/controllers/strom/internals.rs
+++ testing-tools/src/controllers/strom/internals.rs
@@ -112,7 +112,7 @@ impl<P: WithWalletProvider> AngstromDevnetNodeInternals<P> {
             block_number,
             block_sync.clone(),
             inital_angstrom_state.pool_manager_addr,
-            false
+            true
         )
         .await;
 
