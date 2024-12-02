246620b68c13aa2f40e011ffc20183c8d094dec3
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-12-02 16:55:01 -0500

    debugging consensus

--- testing-tools/src/controllers/enviroments/testnet.rs
+++ testing-tools/src/controllers/enviroments/testnet.rs
@@ -78,10 +78,10 @@ where
                 let provider = initializer.provider_mut().provider_mut();
                 provider.deploy_pool_full().await?;
 
-                provider
-                    .rpc_provider()
-                    .anvil_mine(Some(U256::from(5)), None)
-                    .await?;
+                // provider
+                //     .rpc_provider()
+                //     .anvil_mine(Some(U256::from(5)), None)
+                //     .await?;
 
                 let initial_state = provider.initialize_state().await?;
                 initial_angstrom_state = Some(initial_state);
