7ae195d38d60213200f3bfd8d97cd54ed541ea65
Will Smith <williamqsmith11@gmail.com>
2024-12-03 16:20:09 -0500

    fix merge conflicts

--- bin/testnet/src/simulations/e2e_orders.rs
+++ bin/testnet/src/simulations/e2e_orders.rs
@@ -22,7 +22,6 @@ use crate::cli::e2e_orders::End2EndOrdersCli;
 
 pub async fn run_e2e_orders(executor: TaskExecutor, cli: End2EndOrdersCli) -> eyre::Result<()> {
     let config = cli.testnet_config.make_config()?;
-    let handle = executor.clone();
 
     let agents = vec![end_to_end_agent];
 
@@ -30,7 +29,7 @@ pub async fn run_e2e_orders(executor: TaskExecutor, cli: End2EndOrdersCli) -> ey
     let testnet = AngstromTestnet::spawn_testnet(NoopProvider::default(), config, agents).await?;
 
     executor
-        .spawn_critical_blocking("testnet", testnet.run_to_completion())
+        .spawn_critical_blocking("testnet", testnet.run_to_completion(executor.clone()))
         .await?;
     Ok(())
 }
