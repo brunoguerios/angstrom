fda0db9a5c151e3c2dff2bf8a4079a16e8b5a618
Will Smith <williamqsmith11@gmail.com>
2024-12-03 13:26:42 -0500

    wi

--- bin/testnet/src/cli/e2e_orders.rs
+++ bin/testnet/src/cli/e2e_orders.rs
@@ -10,11 +10,11 @@ pub struct End2EndOrdersCli {
     /// the amount of book orders per pool to generate. 5..10 is 5 to 10 orders
     /// per pool
     #[clap(short, long)]
-    order_amount_range: Range<usize>,
+    pub order_amount_range: Range<usize>,
     /// the range percent in which partial orders will generate, eg 0.05..0.1 is
     /// 5 - 10% of orders will be partial orders
     #[clap(short, long)]
-    partial_pct_range:  Range<f64>,
+    pub partial_pct_range:  Range<f64>,
     #[clap(flatten)]
-    testnet_config:     TestnetCli
+    pub testnet_config:     TestnetCli
 }
--- bin/testnet/src/simulations/e2e_orders.rs
+++ bin/testnet/src/simulations/e2e_orders.rs
@@ -10,4 +10,15 @@ use testing_tools::{
 use tracing::{debug, info};
 
 use crate::cli::e2e_orders::End2EndOrdersCli;
-pub async fn run_e2e_orders(executor: TaskExecutor, cli: End2EndOrdersCli) -> eyre::Result<()> {}
+
+pub async fn run_e2e_orders(executor: TaskExecutor, cli: End2EndOrdersCli) -> eyre::Result<()> {
+    let config = cli.testnet_config.make_config()?;
+
+    // spawn testnet
+    let testnet = AngstromTestnet::spawn_testnet(NoopProvider::default(), config).await?;
+
+    executor
+        .spawn_critical_blocking("testnet", testnet.run_to_completion())
+        .await?;
+    Ok(())
+}
