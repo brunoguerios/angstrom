7afe79664c0e53b0c617f3e12045bcc0e625b391
Will Smith <williamqsmith11@gmail.com>
2024-12-05 10:28:33 -0500

    w

--- testing-tools/src/order_generator/pool_order_generator.rs
+++ testing-tools/src/order_generator/pool_order_generator.rs
@@ -21,9 +21,9 @@ impl PoolOrderGenerator {
     pub fn new(pool_id: PoolId, pool_data: SyncedUniswapPool, block_number: u64) -> Self {
         let price = pool_data.read().unwrap().calculate_price();
 
-        // bounds of 50% from start with a std of 3%
+        // bounds of 50% from start with a std of 10%
         let mut price_distribution =
-            PriceDistribution::new(price, price * 1.5, price * 0.5, price * 0.03);
+            PriceDistribution::new(price, price * 1.5, price * 0.5, price * 0.1);
         let cur_price = price_distribution.generate_price();
         let builder = OrderBuilder::new(pool_data);
 
