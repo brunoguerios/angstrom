bc36c7b6001005962e2b35f275bfe7bee99126b7
Will Smith <williamqsmith11@gmail.com>
2024-12-04 21:16:27 -0500

    test

--- testing-tools/src/contracts/anvil.rs
+++ testing-tools/src/contracts/anvil.rs
@@ -127,9 +127,6 @@ where
     N: Network
 {
     async fn deploy_pending(self) -> eyre::Result<PendingTransaction> {
-        // check to see if transaction will pass first. else error
-        let _ = self.clone().gas(50e6 as u64).call_raw().await?;
-
         Ok(self.gas(50e6 as u64).send().await?.register().await?)
     }
 
