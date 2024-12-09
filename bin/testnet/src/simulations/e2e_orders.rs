9eb03e34a870501cda5af66129bdd0896664c7fd
Will Smith <williamqsmith11@gmail.com>
2024-12-09 14:32:28 -0500

    w

--- bin/testnet/src/simulations/e2e_orders.rs
+++ bin/testnet/src/simulations/e2e_orders.rs
@@ -46,7 +46,7 @@ fn end_to_end_agent<'a>(
             agent_config.uniswap_pools.clone(),
             agent_config.current_block,
             20..50,
-            0.05..0.1
+            0.50..0.7
         );
 
         let mut stream =
