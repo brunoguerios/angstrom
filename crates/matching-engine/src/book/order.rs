15731346f2cfec113329990f54c71f4b3b384e8d
David Bresnick <david@sorellalabs.xyz>
2024-12-11 18:23:47 -0500

    Comments and dead code removal

--- crates/matching-engine/src/book/order.rs
+++ crates/matching-engine/src/book/order.rs
@@ -1,6 +1,6 @@
 use angstrom_types::{
-    matching::{uniswap::PoolPriceVec, CompositeOrder, Debt, TokenQuantity},
-    orders::{OrderFillState, OrderID, OrderId, OrderPrice, OrderVolume},
+    matching::{uniswap::PoolPriceVec, CompositeOrder, Debt},
+    orders::{OrderID, OrderId, OrderPrice, OrderVolume},
     sol_bindings::grouped_orders::{FlashVariants, GroupedVanillaOrder, StandingVariants}
 };
 
@@ -92,6 +92,8 @@ impl<'a, 'b> OrderContainer<'a, 'b> {
         }
     }
 
+    /// Gets the amount a composite order needs to self-fill in order to move to
+    /// a new target price
     pub fn negative_quantity(&self, target_price: OrderPrice) -> OrderVolume {
         match self {
             Self::Composite(c) => c.negative_quantity(target_price.into()),
@@ -99,7 +101,7 @@ impl<'a, 'b> OrderContainer<'a, 'b> {
         }
     }
 
-    /// Retrieve the price for a given order
+    /// Retrieve the starting price bound for a given order
     pub fn price(&self) -> OrderPrice {
         match self {
             Self::BookOrder(o) => o.price().into(),
@@ -125,70 +127,4 @@ impl<'a, 'b> OrderContainer<'a, 'b> {
             }
         }
     }
-}
-
-#[derive(Clone, Debug)]
-pub enum Order<'a> {
-    Flash(FlashVariants),
-    Standing(StandingVariants),
-    AMM(PoolPriceVec<'a>)
-}
-
-impl<'a> Order<'a> {
-    /// Determine if this is an AMM order
-    pub fn is_amm(&self) -> bool {
-        matches!(self, Self::AMM(_))
-    }
-
-    pub fn id(&self) -> Option<OrderID> {
-        match self {
-            Self::Flash(_) => Some(0),
-            Self::Standing(_) => Some(0),
-            _ => None
-        }
-    }
-
-    /// Retrieve the quantity available within the bounds of a given order
-    pub fn quantity(&self, limit_price: OrderPrice) -> OrderVolume {
-        match self {
-            Self::Flash(lo) => match lo {
-                FlashVariants::Exact(e) => e.amount,
-                FlashVariants::Partial(p) => p.max_amount_in
-            },
-            Self::Standing(lo) => match lo {
-                StandingVariants::Exact(e) => e.amount,
-                StandingVariants::Partial(p) => p.max_amount_in
-            },
-            Self::AMM(ammo) => ammo.quantity(limit_price).0
-        }
-    }
-
-    // /// Retrieve the price for a given order
-    // pub fn price(&self) -> OrderPrice {
-    //     match self {
-    //         Self::KillOrFill(lo) => lo.min_price,
-    //         Self::PartialFill(lo) => lo.min_price,
-    //         Self::AMM(ammo) => ammo.start_bound.as_u256()
-    //     }
-    // }
-
-    // /// Produce a new order representing the remainder of the current order
-    // /// after the fill operation has been performed
-    // pub fn fill(&self, filled_quantity: OrderVolume) -> Self {
-    //     match self {
-    //         Self::Flash(lo) => Self::Flash(FlashOrder {
-    //             max_amount_in_or_out: lo.max_amount_in_or_out - filled_quantity,
-    //             ..lo.clone()
-    //         }),
-    //         Self::Standing(lo) => Self::PartialFill(StandingOrder {
-    //             max_amount_in_or_out: lo.max_amount_in_or_out - filled_quantity,
-    //             ..lo.clone()
-    //         }),
-    //         Self::AMM(r) => {
-    //             r.fill(filled_quantity);
-    //             // Return a bogus order that we never use
-    //             Self::PartialFill(StandingOrder::default())
-    //         }
-    //     }
-    // }
 }
