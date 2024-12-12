bab1aa8c7a97d882ad38ef6d5b6f2f2b3ddc1045
Will Smith <williamqsmith11@gmail.com>
2024-12-12 16:01:43 -0500

    w

--- crates/validation/src/common/token_pricing.rs
+++ crates/validation/src/common/token_pricing.rs
@@ -68,7 +68,7 @@ impl TokenPriceGenerator {
                     };
 
                     for block_number in
-                        current_block.saturating_sub(blocks_to_avg_price)..=current_block
+                        current_block.saturating_sub(blocks_to_avg_price)..current_block
                     {
                         tracing::debug!(block_number, current_block, ?pool_key, "loading pool");
                         let pool_data = data_loader
