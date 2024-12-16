ebf9c72f3172be4fa1d9871131fe70666b8d08ed
Will Smith <williamqsmith11@gmail.com>
2024-12-16 14:19:26 -0500

    fmt + clippy

--- crates/matching-engine/src/lib.rs
+++ crates/matching-engine/src/lib.rs
@@ -43,18 +43,10 @@ pub trait MatchingEngineHandle: Send + Sync + Clone + Unpin + 'static {
 pub fn build_book(id: PoolId, amm: Option<PoolSnapshot>, orders: HashSet<BookOrder>) -> OrderBook {
     let (mut bids, mut asks): (Vec<BookOrder>, Vec<BookOrder>) =
         orders.into_iter().partition(|o| o.is_bid);
-    // assert bids decreasing and asks increasing
-    bids.sort_by(|a, b| b.limit_price().cmp(&a.limit_price()));
-    asks.sort_by(|a, b| a.limit_price().cmp(&b.limit_price()));
 
-    assert!(
-        bids.is_sorted_by(|a, b| a.limit_price() >= b.limit_price()),
-        "bids aren't decreasing by price"
-    );
-    assert!(
-        asks.is_sorted_by(|a, b| a.limit_price() <= b.limit_price()),
-        "asks aren't increasing by price"
-    );
+    // assert bids decreasing and asks increasing
+    bids.sort_by_key(|b| std::cmp::Reverse(b.limit_price()));
+    asks.sort_by_key(|a| a.limit_price());
 
     OrderBook::new(id, amm, bids, asks, Some(book::sort::SortStrategy::ByPriceByVolume))
 }
