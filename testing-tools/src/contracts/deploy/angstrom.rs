3b7221558b274f2774415ca758fe42c75ddf3249
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-01-15 16:09:30 -0500

    testing

--- testing-tools/src/contracts/deploy/angstrom.rs
+++ testing-tools/src/contracts/deploy/angstrom.rs
@@ -1,4 +1,5 @@
 use alloy::{contract::RawCallBuilder, primitives::Address, sol_types::SolValue};
+use alloy_primitives::Bytes;
 use angstrom_types::contract_bindings::angstrom::Angstrom;
 
 use super::{mine_address, uniswap_flags::UniswapFlags, DEFAULT_CREATE2_FACTORY};
@@ -46,7 +47,7 @@ pub async fn deploy_angstrom_with_factory<
         mine_address(flags, UniswapFlags::mask(), mock_builder.calldata());
     println!("mock_builder.calldata(): {:?}", mock_builder.calldata());
     let final_mock_initcode = [salt.abi_encode(), mock_builder.calldata().to_vec()].concat();
-    println!("final_mock_initcode: {:?}", final_mock_initcode);
+    println!("final_mock_initcode: {:?}", Bytes::from(final_mock_initcode));
     RawCallBuilder::new_raw(&provider, final_mock_initcode.into())
         .to(factory)
         .gas(50e6 as u64)
