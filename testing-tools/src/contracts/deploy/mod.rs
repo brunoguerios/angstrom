b94fcd0e1e12df6d540f08b2494ad841f83921ba
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-01-16 16:45:55 -0500

    merged

--- testing-tools/src/contracts/deploy/mod.rs
+++ testing-tools/src/contracts/deploy/mod.rs
@@ -14,55 +14,11 @@ const DEFAULT_CREATE2_FACTORY: Address = address!("4e59b44847b379578588920cA78Fb
 /// Attempt to find a target address that includes the appropriate flags
 /// Returns the address found and the salt needed to pad the initcode to
 /// deploy to that address
-pub fn mine_address(
-    deployer: Address,
-    flags: U160,
-    mask: U160,
-    initcode: &Bytes
-) -> (Address, U256) {
-    // this one DOES NOT works with create3
-    mine_address_with_factory_create3(deployer, DEFAULT_CREATE2_FACTORY, flags, mask);
-
-    // this one works with create2
-    mine_address_with_factory_create2(DEFAULT_CREATE2_FACTORY, flags, mask, initcode)
+pub fn mine_address(flags: U160, mask: U160, initcode: &Bytes) -> (Address, U256) {
+    mine_address_with_factory(DEFAULT_CREATE2_FACTORY, flags, mask, initcode)
 }
 
-pub fn mine_address_with_factory_create3(
-    deployer: Address,
-    factory: Address,
-    flags: U160,
-    mask: U160
-) -> (Address, U256) {
-    let mut salt = U256::ZERO;
-    let mut counter: u128 = 0;
-    loop {
-        // i tried both variants of these
-        let target_address: Address = sanity_calculate(deployer, salt.to_le_bytes());
-        // let target_address: Address = sanity_calculate(factory, salt.to_le_bytes());
-        let u_address: U160 = target_address.into();
-        if (u_address & mask) == flags {
-            break;
-        }
-        salt += U256::from(1_u8);
-        counter += 1;
-        if counter > 100_000 {
-            panic!("We tried this too many times!")
-        }
-    }
-    // i tried both variants of these
-    let final_address = sanity_calculate(deployer, salt.to_le_bytes());
-    // let final_address: Address = sanity_calculate(factory, salt.to_le_bytes());
-
-    (final_address, salt)
-}
-
-fn sanity_calculate(deployer: Address, salt: [u8; 32]) -> Address {
-    // i tried both variants of these
-    calc_addr(&**deployer, &salt).into()
-    // calc_addr_with_bytes(&**deployer, &salt).into()
-}
-
-pub fn mine_address_with_factory_create2(
+pub fn mine_address_with_factory(
     factory: Address,
     flags: U160,
     mask: U160,
@@ -85,19 +41,7 @@ pub fn mine_address_with_factory_create2(
     }
     let final_address = factory.create2(B256::from(salt), init_code_hash);
     (final_address, salt)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::uniswap_flags::UniswapFlags;
-
-    #[test]
-    fn test_deploy_addresses() {
-        let flags = UniswapFlags::BeforeSwap
-            | UniswapFlags::BeforeInitialize
-            | UniswapFlags::BeforeAddLiquidity
-            | UniswapFlags::BeforeRemoveLiquidity;
-    }
+    // (ANGSTROM_ADDRESS, U256::from(ANGSTROM_ADDRESS_SALT))
 }
 
 pub fn mine_create3_address(owner: Address) -> (Address, U256, u8) {
