0b13227cdecb943851e91e074ec39d9c4114c1ab
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-01-21 13:14:37 -0500

    cleanup

--- testing-tools/src/contracts/deploy/angstrom.rs
+++ testing-tools/src/contracts/deploy/angstrom.rs
@@ -6,59 +6,6 @@ use angstrom_types::contract_bindings::angstrom::Angstrom;
 
 use super::{mine_create3_address, SUB_ZERO_FACTORY};
 
-// pub async fn deploy_angstrom<
-//     T: alloy::contract::private::Transport + ::core::clone::Clone,
-//     P: alloy::contract::private::Provider<T, N> +
-// alloy::providers::WalletProvider<N>,     N: alloy::contract::private::Network
-// >(
-//     provider: &P,
-//     pool_manager: Address,
-//     controller: Address,
-//     feemaster: Address
-// ) -> Address {
-//     deploy_angstrom_with_factory(
-//         provider,
-//         pool_manager,
-//         DEFAULT_CREATE2_FACTORY,
-//         controller,
-//         feemaster
-//     )
-//     .await
-// }
-
-// pub async fn deploy_angstrom_with_factory<
-//     T: alloy::contract::private::Transport + ::core::clone::Clone,
-//     P: alloy::contract::private::Provider<T, N> +
-// alloy::providers::WalletProvider<N>,     N: alloy::contract::private::Network
-// >(
-//     provider: &P,
-//     pool_manager: Address,
-//     factory: Address,
-//     controller: Address,
-//     _feemaster: Address
-// ) -> Address {
-//     // Setup our flags and mask
-//     let flags = UniswapFlags::BeforeSwap
-//         | UniswapFlags::BeforeInitialize
-//         | UniswapFlags::BeforeAddLiquidity
-//         | UniswapFlags::BeforeRemoveLiquidity;
-
-//     let mock_builder = Angstrom::deploy_builder(&provider, pool_manager,
-// controller);     let (mock_tob_address, salt) =
-//         mine_address(flags, UniswapFlags::mask(), mock_builder.calldata());
-//     let final_mock_initcode = [salt.abi_encode(),
-// mock_builder.calldata().to_vec()].concat();     RawCallBuilder::new_raw(&
-// provider, final_mock_initcode.into())         .to(factory)
-//         .gas(50e6 as u64)
-//         .send()
-//         .await
-//         .unwrap()
-//         .watch()
-//         .await
-//         .unwrap();
-//     mock_tob_address
-// }
-
 pub async fn deploy_angstrom_create3<
     T: alloy::contract::private::Transport + ::core::clone::Clone,
     P: alloy::contract::private::Provider<T, Ethereum> + alloy::providers::WalletProvider<Ethereum>
