7bd50071cd70f02f3c4ba0a4d4ebd8a5f07b3bf3
Will Smith <williamqsmith11@gmail.com>
2025-01-21 11:05:16 -0500

    w

--- crates/validation/src/order/sim/gas.rs
+++ crates/validation/src/order/sim/gas.rs
@@ -129,7 +129,7 @@ where
         let bundle = bundle.pade_encode();
 
         // if exact in is true, we fail?????
-        let (amount_in, amount_out) = if order.exact_in() {
+        let (amount_in, amount_out) = if order.token_in() < order.token_out() {
             (U256::from(order.amount_in()), {
                 let price = Ray::from(U256::from(order.limit_price()));
                 price.mul_quantity(U256::from(order.amount_in()))
