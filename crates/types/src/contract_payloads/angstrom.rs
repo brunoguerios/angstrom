55ecc511c865f06f4407e11547b552a3710dbeb8
Will Smith <williamqsmith11@gmail.com>
2025-01-21 11:44:31 -0500

    w

--- crates/types/src/contract_payloads/angstrom.rs
+++ crates/types/src/contract_payloads/angstrom.rs
@@ -630,21 +630,6 @@ impl AngstromBundle {
         let mut user_orders = Vec::new();
         let mut asset_builder = AssetBuilder::new();
 
-        // asset_builder.allocate(
-        //     AssetBuilderStage::UserOrder,
-        //     user_order.token_in(),
-        //     {
-        //
-        //     if user_order.exact_in() {
-        //         let price = Ray::from(user_order.limit_price());
-        //         price.mul_quantity(U256::from(user_order.amount_in())).to()
-        //     } else {
-        //         user_order.amount_in()
-        //     }
-        //     }
-        //     user_order.amount_in()
-        // );
-
         asset_builder.allocate(AssetBuilderStage::UserOrder, user_order.token_out(), {
             if user_order.exact_in() {
                 let price = Ray::from(user_order.limit_price());
