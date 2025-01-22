0bf7ac5a65a9bbf9937188a37a76adda75e5b729
Will Smith <williamqsmith11@gmail.com>
2025-01-22 13:53:40 -0500

    w

--- bin/testnet/src/simulations/e2e_orders.rs
+++ bin/testnet/src/simulations/e2e_orders.rs
@@ -45,7 +45,7 @@ fn end_to_end_agent<'a>(
         let mut generator = OrderGenerator::new(
             agent_config.uniswap_pools.clone(),
             agent_config.current_block,
-            10..15,
+            5..10,
             0.1..0.6
         );
 
