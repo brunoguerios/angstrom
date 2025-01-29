970626467b1f4b59fae360c6977c8907844e71aa
David Bresnick <david@sorellalabs.xyz>
2025-01-29 18:44:41 -0500

    Update log builder for tracing

--- bin/testnet/src/cli/mod.rs
+++ bin/testnet/src/cli/mod.rs
@@ -10,7 +10,7 @@ use testing_tools::types::config::{DevnetConfig, TestnetConfig};
 use testnet::TestnetCli;
 use tracing::Level;
 use tracing_subscriber::{
-    layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer, Registry
+    filter, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer, Registry
 };
 
 use crate::{run_devnet, run_testnet, simulations::e2e_orders::run_e2e_orders};
@@ -67,33 +67,32 @@ impl AngstromTestnetCli {
             _ => Level::TRACE
         };
 
-        let layers = vec![
-            layer_builder(format!("testnet={level}")),
-            layer_builder(format!("devnet={level}")),
-            layer_builder(format!("angstrom_rpc={level}")),
-            layer_builder(format!("angstrom={level}")),
-            layer_builder(format!("testing_tools={level}")),
-            layer_builder(format!("matching_engine={level}")),
-            layer_builder(format!("uniswap_v4={level}")),
-            layer_builder(format!("consensus={level}")),
-            layer_builder(format!("validation={level}")),
-            layer_builder(format!("order_pool={level}")),
-        ];
+        let envfilter = filter::EnvFilter::builder().try_from_env().ok();
+        let format = tracing_subscriber::fmt::layer()
+            .with_ansi(true)
+            .with_target(true);
 
-        tracing_subscriber::registry().with(layers).init();
+        if let Some(f) = envfilter {
+            tracing_subscriber::registry().with(format).with(f).init();
+        } else {
+            let filter = filter::Targets::new()
+                .with_target("testnet", level)
+                .with_target("devnet", level)
+                .with_target("angstrom_rpc", level)
+                .with_target("angstrom", level)
+                .with_target("testing_tools", level)
+                .with_target("matching_engine", level)
+                .with_target("uniswap_v4", level)
+                .with_target("consensus", level)
+                .with_target("validation", level)
+                .with_target("order_pool", level);
+
+            tracing_subscriber::registry()
+                .with(format)
+                .with(filter)
+                .init();
+        }
     }
-}
-
-fn layer_builder(filter_str: String) -> Box<dyn Layer<Registry> + Send + Sync> {
-    let filter = EnvFilter::builder()
-        .with_default_directive(filter_str.parse().unwrap())
-        .from_env_lossy();
-
-    tracing_subscriber::fmt::layer()
-        .with_ansi(true)
-        .with_target(true)
-        .with_filter(filter)
-        .boxed()
 }
 
 #[derive(Debug, Subcommand, Clone)]
