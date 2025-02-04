0c872d305e1b5c1243265e4bdbdd521024a2bcd3
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-02-04 11:00:43 -0500

    updated config store

--- testing-tools/src/providers/initializer.rs
+++ testing-tools/src/providers/initializer.rs
@@ -227,13 +227,13 @@ impl AnvilInitializer {
             )
             .from(self.provider.controller())
             .nonce(nonce)
-            .run_safe()
-            .await
-            .unwrap();
-        // .deploy_pending()
-        // .await?;
-        tracing::debug!("success: controller_configure_pool");
-        // self.pending_state.add_pending_tx(controller_configure_pool);
+            // .run_safe()
+            // .await
+            // .unwrap();
+            .deploy_pending()
+            .await?;
+        // tracing::debug!("success: controller_configure_pool");
+        self.pending_state.add_pending_tx(controller_configure_pool);
 
         tracing::debug!("initializing pool");
         let i = self
@@ -241,35 +241,35 @@ impl AnvilInitializer {
             .initializePool(pool_key.currency0, pool_key.currency1, store_index, *price)
             .from(self.provider.controller())
             .nonce(nonce + 1)
-            .run_safe()
-            .await
-            .unwrap();
-        // .deploy_pending()
-        // .await?;
-        tracing::debug!("success: angstrom.initializePool");
-        // self.pending_state.add_pending_tx(i);
+            // .run_safe()
+            // .await
+            // .unwrap();
+            .deploy_pending()
+            .await?;
+        // tracing::debug!("success: angstrom.initializePool");
+        self.pending_state.add_pending_tx(i);
 
         tracing::debug!("tick spacing");
         let pool_gate = self
             .pool_gate
             .tickSpacing(pool_key.tickSpacing)
             .from(self.provider.controller())
-            .run_safe()
-            .await
-            .unwrap();
-        // .deploy_pending()
-        // .await?;
-        tracing::debug!("success: pool_gate");
-        // self.pending_state.add_pending_tx(pool_gate);
+            // .run_safe()
+            // .await
+            // .unwrap();
+            .deploy_pending()
+            .await?;
+        // tracing::debug!("success: pool_gate");
+        self.pending_state.add_pending_tx(pool_gate);
 
-        let config_store = AngstromPoolConfigStore::load_from_chain(
-            *self.angstrom.address(),
-            BlockId::latest(),
-            &self.provider.provider()
-        )
-        .await
-        .unwrap();
-        tracing::error!("CONFIG STORE: {:?}", config_store);
+        // let config_store = AngstromPoolConfigStore::load_from_chain(
+        //     *self.angstrom.address(),
+        //     BlockId::latest(),
+        //     &self.provider.provider()
+        // )
+        // .await
+        // .unwrap();
+        // tracing::error!("CONFIG STORE: {:?}", config_store);
 
         let mut rng = thread_rng();
 
