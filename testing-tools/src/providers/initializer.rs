591a8ec237614f4683868e0ebffb8827a6a0125f
Will Smith <williamqsmith11@gmail.com>
2025-02-04 14:09:04 -0500

    w

--- testing-tools/src/providers/initializer.rs
+++ testing-tools/src/providers/initializer.rs
@@ -240,13 +240,13 @@ impl AnvilInitializer {
             .initializePool(pool_key.currency0, pool_key.currency1, store_index, *price)
             .from(self.provider.controller())
             .nonce(nonce + 1)
-            .run_with_results_safe()
-            .await
-            .unwrap();
-        // .deploy_pending()
-        // .await?;
+            // .run_with_results_safe()
+            // .await
+            // .unwrap();
+            .deploy_pending()
+            .await?;
         // tracing::debug!("success: angstrom.initializePool");
-        // self.pending_state.add_pending_tx(initialize_angstrom_pool);
+        self.pending_state.add_pending_tx(initialize_angstrom_pool);
 
         tracing::debug!("tick spacing");
         let pool_gate = self
