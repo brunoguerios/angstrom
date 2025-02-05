434f4eaa0910ef6f1bc1e624a2a15ad0d1715c20
Will Smith <williamqsmith11@gmail.com>
2025-02-05 14:29:23 -0500

    w

--- bin/testnet/src/simulations/matching_comp.rs
+++ bin/testnet/src/simulations/matching_comp.rs
@@ -61,8 +61,8 @@ fn cmp_agent<'a>(
         let mut generator = OrderGenerator::new(
             agent_config.uniswap_pools.clone(),
             agent_config.current_block,
-            5..10,
-            0.1..0.6
+            10..20,
+            0.2..0.6
         );
 
         let mut stream =
