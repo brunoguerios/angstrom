5d067fe851e3a931f842ca5639a5dc8433561377
philogy <philippe@dumo.net>
2025-02-05 23:24:32 +0000

    new mock

--- contracts/test/_mocks/MintableMockERC20.sol
+++ contracts/test/_mocks/MintableMockERC20.sol
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {MockERC20} from "super-sol/mocks/MockERC20.sol";
+import {ERC20} from "solady/src/tokens/ERC20.sol";
 
-contract MintableMockERC20 is MockERC20 {}
+contract MintableMockERC20 is ERC20 {
+    string internal _name = "Mock Token";
+    string internal _symbol = "MCK";
+
+    function setMeta(string memory newName, string memory newSymbol) external {
+        _name = newName;
+        _symbol = newSymbol;
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external {
+        _burn(from, amount);
+    }
+
+    function name() public view override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view override returns (string memory) {
+        return _symbol;
+    }
+}
