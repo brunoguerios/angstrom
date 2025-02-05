4d0cc989b055a136c77f140e4d6a7b05162f6143
Will Smith <williamqsmith11@gmail.com>
2025-02-05 13:53:55 -0500

    w

--- bin/testnet/src/cli/compare_engines.rs
+++ bin/testnet/src/cli/compare_engines.rs
@@ -4,7 +4,7 @@ use super::testnet::TestnetCli;
 
 #[derive(Debug, Clone, Parser)]
 pub struct CompareEnginesCli {
-    #[arg(short = 'a', default_value_t = false)]
+    #[arg(short = 'z', default_value_t = false)]
     pub include_amm:    bool,
     #[clap(flatten)]
     pub testnet_config: TestnetCli
