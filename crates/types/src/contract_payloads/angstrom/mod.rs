27e9c6795aabe53cccf1e391d5ce2038bb2f9e40
David Bresnick <david@sorellalabs.xyz>
2025-02-11 15:18:15 -0500

    Clean up quantities and comments

--- crates/types/src/contract_payloads/angstrom/mod.rs
+++ crates/types/src/contract_payloads/angstrom/mod.rs
@@ -74,36 +74,29 @@ impl AngstromBundle {
             };
 
             // need to recover sender from signature
-            // We use block_number + 1 here because the order was submitted for the next
-            // block
             let hash = order.signing_hash(&self.pairs, &self.assets, block_number);
             let address = order.signature.recover_signer(hash);
 
             // Grab the price because we need it most of the time
             let price = Ray::from(self.pairs[order.pair_index as usize].price_1over0);
-            let qty = if order.zero_for_one {
-                if order.exact_in {
-                    // zero for 1 and exact in - the order is in t0 context and the quantity on the
-                    // order is how much we'll be taking from the user
-                    order.order_quantities.fetch_max_amount() + order.extra_fee_asset0
-                } else {
-                    // zero for 1 and exact out - The quantity on the order is how much T1 we will
-                    // be returning to the user, we have to calculate how much T0 to take
-                    // Given our output T1, calculate how much T0 needs to be input, round up, add
-                    // our extra fee in T0 format
+            let qty = match (order.zero_for_one, order.exact_in) {
+                // Zero for one, exact in -> quantity on the order (t0) + extra fee (t0)
+                (true, true) => order.order_quantities.fetch_max_amount() + order.extra_fee_asset0,
+                // Zero for one, exact out -> quantity needed to produce output amount (t0) + extra
+                // fee (t0)
+                (true, false) => {
                     price.inverse_quantity(order.order_quantities.fetch_max_amount(), true)
                         + order.extra_fee_asset0
                 }
-            } else if order.exact_in {
-                // 1 for 0 and exact in - the order is in T1 context and the quantity on the
-                // order is how much we'll be taking from the user
-                order.order_quantities.fetch_max_amount()
-            } else {
-                // 1 for 0 and exact out - the order is in T0 context and the quantity on the
-                // order is how much T0 we'll be returning
-                let total_conversion_needed =
-                    order.order_quantities.fetch_max_amount() + order.extra_fee_asset0;
-                price.quantity(total_conversion_needed, true)
+                // One for zero, exact in -> quantity on the order (t1) and fee is taken from the
+                // ouptut
+                (false, true) => order.order_quantities.fetch_max_amount(),
+                // One for zero, exact out -> quantity needed to produce the output amount + fee
+                // (t1)
+                (false, false) => price.quantity(
+                    order.order_quantities.fetch_max_amount() + order.extra_fee_asset0,
+                    true
+                )
             };
 
             tracing::info!(?token, from_address = ?address, qty, "Building user order override");
@@ -131,8 +124,6 @@ impl AngstromBundle {
             };
 
             // need to recover sender from signature
-            // We use block_number + 1 here because the order was submitted for the next
-            // block
             let hash = order.signing_hash(&self.pairs, &self.assets, block_number);
             let address = order.signature.recover_signer(hash);
 
@@ -215,7 +206,7 @@ impl AngstromBundle {
         user_order: &OrderWithStorageData<RpcTopOfBlockOrder>
     ) -> eyre::Result<Self> {
         let mut top_of_block_orders = Vec::new();
-        let mut pool_updates = Vec::new();
+        let pool_updates = Vec::new();
         let mut pairs = Vec::new();
         let user_orders = Vec::new();
         let mut asset_builder = AssetBuilder::new();
