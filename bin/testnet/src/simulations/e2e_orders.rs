93ec4aa0b773993f27c7c5bd63db64b8d1cd0ba4
Will Smith <williamqsmith11@gmail.com>
2025-03-12 17:13:23 -0400

    feat: add test to ensure bundles are landing

--- bin/testnet/src/simulations/e2e_orders.rs
+++ bin/testnet/src/simulations/e2e_orders.rs
@@ -96,3 +96,93 @@ fn end_to_end_agent<'a>(
         Ok(())
     }) as Pin<Box<dyn Future<Output = eyre::Result<()>> + Send + 'a>>
 }
+
+#[cfg(test)]
+pub mod test {
+
+    use std::time::Duration;
+
+    use alloy::{consensus::BlockHeader, providers::Provider};
+    use alloy_rpc_types::{BlockTransactionsKind, TransactionTrait};
+    use angstrom_types::{
+        contract_payloads::angstrom::AngstromBundle, primitive::TESTNET_ANGSTROM_ADDRESS
+    };
+    use futures::{FutureExt, StreamExt};
+    use pade::PadeDecode;
+    use reth_tasks::{TaskSpawner, TokioTaskExecutor};
+    use testing_tools::contracts::anvil::WalletProviderRpc;
+    use tokio::time::timeout;
+
+    use super::*;
+    use crate::cli::{init_tracing, testnet::TestnetCli};
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn testnet_lands_block() {
+        init_tracing(4);
+        let config = TestnetCli {
+            eth_fork_url: "wss://ethereum-rpc.publicnode.com".to_string(),
+            nodes_in_network: 4,
+            ..Default::default()
+        };
+
+        let config = config.make_config().unwrap();
+
+        let agents = vec![end_to_end_agent];
+        tracing::info!("spinning up e2e nodes for angstrom");
+
+        // spawn testnet
+        let testnet = AngstromTestnet::spawn_testnet(NoopProvider::default(), config, agents)
+            .await
+            .expect("failed to start angstrom testnet");
+
+        // grab provider so we can query from the chain later.
+        let provider = testnet.node_provider(None).rpc_provider();
+
+        let executor = TokioTaskExecutor::default();
+        let task = executor.spawn_critical_blocking(
+            "testnet",
+            testnet.run_to_completion(executor.clone()).boxed()
+        );
+
+        assert!(
+            timeout(Duration::from_secs(60 * 3), wait_for_valid_block(provider))
+                .await
+                .is_ok()
+        );
+    }
+
+    async fn wait_for_valid_block(provider: WalletProviderRpc) {
+        // once started, it might take 2-3 blocks for a bundle with user orders to land.
+        // we want to verify that both tob + user orders land.
+        let mut sub = provider
+            .subscribe_blocks()
+            .await
+            .expect("failed to subscribe to blocks");
+        while let Ok(next) = sub.recv().await {
+            let bn = next.number();
+            let block = provider
+                .get_block(alloy_rpc_types::BlockId::Number(bn.into()), BlockTransactionsKind::Full)
+                .await
+                .unwrap()
+                .unwrap();
+            if block
+                .transactions
+                .into_transactions_vec()
+                .into_iter()
+                .filter(|tx| tx.to() == Some(TESTNET_ANGSTROM_ADDRESS))
+                .filter_map(|tx| {
+                    let calldata = tx.input().to_vec();
+                    let mut slice = calldata.as_slice();
+                    let data = &mut slice;
+                    let bundle: AngstromBundle = PadeDecode::pade_decode(data, None).unwrap();
+                    (!(bundle.top_of_block_orders.is_empty() || bundle.user_orders.is_empty()))
+                        .then_some(true)
+                })
+                .count()
+                != 0
+            {
+                break;
+            }
+        }
+    }
+}
