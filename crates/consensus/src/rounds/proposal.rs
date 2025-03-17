f94237736c220acf388d8a751c6c67863417885d
Will Smith <williamqsmith11@gmail.com>
2025-03-17 09:57:58 -0400

    update logs

--- crates/consensus/src/rounds/proposal.rs
+++ crates/consensus/src/rounds/proposal.rs
@@ -128,10 +128,11 @@ impl ProposalState {
                 .await;
 
             let (hash, success) = provider.sign_and_send(signer, tx).await;
-            tracing::info!("submitted bundle");
             if !success {
+                tracing::info!("submission failed");
                 return false;
             }
+            tracing::info!("submitted bundle");
 
             // wait for next block. then see if transaction landed
             provider
