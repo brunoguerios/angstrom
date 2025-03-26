a0e190718612cabea752a575e75f8638e9703b2f
Sam Johnson <sam@durosoft.com>
2025-03-26 16:31:00 -0400

    rip out ctrlc

--- bin/testnet/src/main.rs
+++ bin/testnet/src/main.rs
@@ -4,15 +4,6 @@
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
 fn main() {
-    println!("starting angstrom...");
-    println!("installing ctrl+c handler...");
-    ctrlc::set_handler(move || {
-        println!();
-        println!("GOT IT");
-        println!();
-    })
-    .expect("error setting ctrl+c handler");
-    println!("ctrl+c handler installed.");
     if let Err(err) = testnet::run() {
         eprintln!("Error: {err:?}");
         std::process::exit(1);
