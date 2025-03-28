b10933f80a29afc9f27e1d2dd230601fde4c2c71
Sam Johnson <sam@durosoft.com>
2025-03-28 12:06:38 -0400

    test MaxGasGreaterThanMin error

--- crates/validation/src/order/state/order_validators/max_gas_lt_min.rs
+++ crates/validation/src/order/state/order_validators/max_gas_lt_min.rs
@@ -17,3 +17,27 @@ impl OrderValidation for EnsureMaxGasLessThanMinAmount {
         }
     }
 }
+
+#[test]
+fn test_max_gas_greater_than_min() {
+    use angstrom_types::{
+        primitive::OrderValidationError, sol_bindings::grouped_orders::StandingVariants
+    };
+
+    use crate::order::{
+        GroupedVanillaOrder,
+        state::order_validators::{
+            EnsureMaxGasLessThanMinAmount, OrderValidationState, make_base_order
+        }
+    };
+
+    let mut order = make_base_order();
+    if let GroupedVanillaOrder::Standing(StandingVariants::Exact(ref mut o)) = order {
+        o.max_extra_fee_asset0 = o.amount + 1;
+    }
+
+    let validator = EnsureMaxGasLessThanMinAmount;
+    let mut state = OrderValidationState::new(&order);
+    let result = validator.validate_order(&mut state);
+    assert_eq!(result, Err(OrderValidationError::MaxGasGreaterThanMinAmount));
+}
