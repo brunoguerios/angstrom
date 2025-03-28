1e68de2773da9809a41509ec64faf4a58398036d
Sam Johnson <sam@durosoft.com>
2025-03-28 12:05:36 -0400

    test NoGasSpecified error

--- crates/validation/src/order/state/order_validators/gas_set.rs
+++ crates/validation/src/order/state/order_validators/gas_set.rs
@@ -17,3 +17,25 @@ impl OrderValidation for EnsureGasSet {
         }
     }
 }
+
+#[test]
+fn test_no_gas_specified() {
+    use angstrom_types::{
+        primitive::OrderValidationError, sol_bindings::grouped_orders::StandingVariants
+    };
+
+    use crate::order::{
+        GroupedVanillaOrder,
+        state::order_validators::{EnsureGasSet, OrderValidationState, make_base_order}
+    };
+
+    let mut order = make_base_order();
+    if let GroupedVanillaOrder::Standing(StandingVariants::Exact(ref mut o)) = order {
+        o.max_extra_fee_asset0 = 0;
+    }
+
+    let validator = EnsureGasSet;
+    let mut state = OrderValidationState::new(&order);
+    let result = validator.validate_order(&mut state);
+    assert_eq!(result, Err(OrderValidationError::NoGasSpecified));
+}
