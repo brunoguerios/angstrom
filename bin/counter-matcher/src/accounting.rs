e56ea1fef20764c73e87f6b113726be0bb2fbe24
Will Smith <williamqsmith11@gmail.com>
2025-04-21 15:32:04 -0400

    more templating

--- bin/counter-matcher/src/accounting.rs
+++ bin/counter-matcher/src/accounting.rs
@@ -1,15 +1,25 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
-use alloy_primitives::Address;
+use alloy_primitives::{Address, B256};
 use angstrom_types::{primitive::AngstromSigner, sol_bindings::grouped_orders::AllOrders};
+use sepolia_bundle_lander::env::ProviderType;
 
 /// used to see what wallet should create the orders
 pub struct WalletAccounting {
-    pk:               AngstromSigner,
-    available_tokens: HashMap<Address, u128>
+    pub pk:              AngstromSigner,
+    on_chain:            HashMap<Address, u128>,
+    // token -> order_id -> amount
+    pending_adjustments: HashMap<Address, HashMap<B256, u128>>
 }
 
 impl WalletAccounting {
+    /// initializes the wallet with the curent
+    pub async fn new(pk: AngstromSigner, provider: Arc<ProviderType>) -> Self {
+        todo!()
+    }
+
+    pub async fn new_block(&mut self, block_number: u64, provider: Arc<ProviderType>) {}
+
     pub fn can_support_amount(&self, order: AllOrders) -> bool {
         true
     }
--- bin/counter-matcher/src/order_manager.rs
+++ bin/counter-matcher/src/order_manager.rs
@@ -1,19 +1,91 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
+use alloy::{
+    signers::{Signer, SignerSync},
+    sol_types::SolValue
+};
 use alloy_primitives::B256;
-use angstrom_types::sol_bindings::grouped_orders::AllOrders;
+use angstrom_rpc::api::OrderApiClient;
+use angstrom_types::{
+    orders::CancelOrderRequest,
+    sol_bindings::{RawPoolOrder, grouped_orders::AllOrders}
+};
 use jsonrpsee::ws_client::WsClient;
+use pade::PadeEncode;
+use sepolia_bundle_lander::env::ProviderType;
+use uniswap_v4::uniswap::{pool::EnhancedUniswapPool, pool_data_loader::DataLoader};
 
 use crate::accounting::WalletAccounting;
 
 /// holds orders that are currently being processed.
 pub struct OrderManager {
-    wallets: Vec<WalletAccounting>,
+    block_number: u64,
+    wallets:      Vec<WalletAccounting>,
+    pools:        Vec<EnhancedUniswapPool>,
+    provider:     Arc<ProviderType>,
 
-    /// order id to index into wallets of the order + order itself.
-    /// we need to track wallet incase we need to cancel it
+    /// our active orders
     active_orders: HashMap<B256, (usize, AllOrders)>,
+    /// map of user order to our active counter order
+    user_orders:   HashMap<B256, (B256, AllOrders)>,
     client:        WsClient
 }
 
-impl OrderManager {}
+impl OrderManager {
+    pub fn new(
+        block_number: u64,
+        provider: Arc<ProviderType>,
+        wallets: Vec<WalletAccounting>,
+        pools: Vec<EnhancedUniswapPool>,
+        client: WsClient
+    ) -> Self {
+        Self { block_number, provider, wallets, pools, client, active_orders: Default::default() }
+    }
+
+    pub async fn new_block(&mut self, block_number: u64) {
+        for pool in &mut self.pools {
+            pool.update_to_block(Some(block_number), self.provider.clone())
+                .await
+                .unwrap();
+        }
+        self.block_number = block_number;
+    }
+
+    /// handles when a new order is pulled from the stream.
+    pub async fn on_new_order(&mut self, order: AllOrders) {
+        let hash = order.order_hash();
+        if order.is_tob() || self.active_orders.contains_key(&hash) {
+            // this is a order we placed
+            return;
+        }
+
+        // see if we have capability to match this
+    }
+
+    pub async fn on_order_cancel(&mut self, order: AllOrders) {
+        let hash = order.order_hash();
+        let Some((our_hash, order)) = self.user_orders.remove(&hash) else { return };
+
+        let Some((wallet, our_order)) = self.active_orders.remove(&our_hash) else { return };
+
+        let order_wallet = &self.wallets[wallet];
+        let addr = order_wallet.pk.address();
+        let signing_bytes = (addr, our_hash).abi_encode_packed();
+        let sig = order_wallet.pk.sign_message_sync(&signing_bytes).unwrap();
+        let encoded_sig = sig.pade_encode();
+
+        let cancel_request = CancelOrderRequest {
+            signature:    encoded_sig.into(),
+            user_address: addr,
+            order_id:     our_hash
+        };
+
+        let cancel_res = self.client.cancel_order(cancel_request).await.unwrap();
+        if !cancel_res {
+            tracing::error!("failed to cancel order");
+            panic!();
+        }
+
+        // if self.active_orders.contains_key(&hash)
+    }
+}
