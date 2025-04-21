7a553eeec66da722260a3c8df75550ee0a08add5
Will Smith <williamqsmith11@gmail.com>
2025-04-21 15:55:45 -0400

    wip: need to update to properly get the max amount

--- bin/counter-matcher/src/accounting.rs
+++ bin/counter-matcher/src/accounting.rs
@@ -1,12 +1,26 @@
 use std::{collections::HashMap, sync::Arc};
 
-use alloy_primitives::{Address, B256};
-use angstrom_types::{primitive::AngstromSigner, sol_bindings::grouped_orders::AllOrders};
+use alloy::{network::TransactionBuilder, providers::Provider, sol_types::SolCall};
+use alloy_primitives::{Address, B256, TxKind};
+use alloy_rpc_types::TransactionRequest;
+use angstrom_types::{
+    primitive::AngstromSigner,
+    sol_bindings::{RawPoolOrder, grouped_orders::AllOrders}
+};
 use sepolia_bundle_lander::env::ProviderType;
+
+// fetch balances.
+alloy::sol!(
+    function balanceOf(address _owner) public view returns (uint256 balance);
+);
+
+/// 20% of our total token for the account can be used.
+const MAX_AMOUNT_PER_TOKEN: f64 = 0.2;
 
 /// used to see what wallet should create the orders
 pub struct WalletAccounting {
     pub pk:              AngstromSigner,
+    tokens:              Vec<Address>,
     on_chain:            HashMap<Address, u128>,
     // token -> order_id -> amount
     pending_adjustments: HashMap<Address, HashMap<B256, u128>>
@@ -14,8 +28,43 @@ pub struct WalletAccounting {
 
 impl WalletAccounting {
     /// initializes the wallet with the curent
-    pub async fn new(pk: AngstromSigner, provider: Arc<ProviderType>) -> Self {
-        todo!()
+    pub async fn new(
+        block_number: u64,
+        pk: AngstromSigner,
+        tokens: Vec<Address>,
+        provider: Arc<ProviderType>
+    ) -> Self {
+        let mut this = Self {
+            pk,
+            tokens,
+            on_chain: Default::default(),
+            pending_adjustments: Default::default()
+        };
+        this.update_balances_for_block(block_number, provider).await;
+
+        this
+    }
+
+    pub async fn update_balances_for_block(
+        &mut self,
+        block_number: u64,
+        provider: Arc<ProviderType>
+    ) {
+        let addr = self.pk.address();
+
+        for token in &self.tokens {
+            let balance = self
+                .make_call(
+                    provider.clone(),
+                    block_number,
+                    addr,
+                    *token,
+                    balanceOfCall::new((addr,))
+                )
+                .await;
+
+            self.on_chain.insert(*token, balance.balance.to::<u128>());
+        }
     }
 
     pub fn add_order(&mut self, token: Address, order_id: B256, amount: u128) {
@@ -33,12 +82,11 @@ impl WalletAccounting {
             .remove(order_id);
     }
 
-    pub async fn new_block(&mut self, block_number: u64, provider: Arc<ProviderType>) {}
-
     /// the order here is the counter matched order.
     /// NOTABLY, we have a max allocation of 20% of the balance of the account
     /// that can be swapped.
     pub fn can_support_amount(&self, order: &AllOrders) -> bool {
+        let token_in = order.token_in();
         true
     }
 
@@ -57,5 +105,26 @@ impl WalletAccounting {
                 amount
             })
             .unwrap_or_default()
+    }
+
+    async fn make_call<TY: SolCall>(
+        &self,
+        provider: Arc<ProviderType>,
+        block_number: u64,
+        from: Address,
+        target: Address,
+        call: TY
+    ) -> TY::Return {
+        let bytes = provider
+            .call(
+                TransactionRequest::default()
+                    .with_from(from)
+                    .with_kind(TxKind::Call(target))
+                    .with_input(call.abi_encode())
+            )
+            .block(block_number.into())
+            .await
+            .unwrap();
+        TY::abi_decode_returns(&bytes, true).unwrap()
     }
 }
