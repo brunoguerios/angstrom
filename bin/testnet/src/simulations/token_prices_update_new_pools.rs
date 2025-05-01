abb3d06f8a2147408e9068479eb367617f0136a1
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-05-01 12:18:50 -0400

    new pools testing

--- bin/testnet/src/simulations/token_prices_update_new_pools.rs
+++ bin/testnet/src/simulations/token_prices_update_new_pools.rs
@@ -29,17 +29,13 @@ pub(crate) async fn run_devnet(executor: TaskExecutor, cli: DevnetCli) -> eyre::
         .strom_validation(|v| v.underlying.token_price_generator());
     let pairs_to_pools = token_gen.pairs_to_pools();
 
-    let mut new_pool_key = initial_state.pool_keys[0].clone();
+    let new_pool_key =
+        PartialConfigPoolKey::new(50, 60, 34028236692, SqrtPriceX96::at_tick(0).unwrap());
 
     testnet.advance_block();
     testnet.check_token_price_gen_has_pools(pairs_to_pools.clone());
     testnet.advance_block();
-    testnet.deploy_new_pool(PartialConfigPoolKey::new(
-        50,
-        60,
-        34028236692,
-        SqrtPriceX96::at_tick(0)
-    ));
+    testnet.deploy_new_pool(new_pool_key);
     testnet.check_token_price_gen_has_pools(pairs_to_pools.clone());
 
     testnet.run().await;
