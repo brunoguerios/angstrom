50e4912c1a9bdd62cbf2ec0721cdb145fbf613d5
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-05-01 14:52:16 -0400

    devnet work

--- bin/testnet/src/simulations/token_prices_update_new_pools.rs
+++ bin/testnet/src/simulations/token_prices_update_new_pools.rs
@@ -34,6 +34,7 @@ pub(crate) async fn run_devnet(executor: TaskExecutor, cli: DevnetCli) -> eyre::
 
     testnet.advance_block();
     testnet.deploy_new_pool(new_pool_key);
+    testnet.advance_block();
     testnet.check_token_price_gen_has_pools(pairs_to_pools.clone());
     // testnet.advance_block();
     // pairs_to_pools.in
