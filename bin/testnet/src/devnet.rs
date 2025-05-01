be1c4455d62c94359864963769095632e302336f
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-05-01 12:36:07 -0400

    devnet init

--- bin/testnet/src/devnet.rs
+++ bin/testnet/src/devnet.rs
@@ -9,9 +9,15 @@ use testing_tools::{
 };
 use tracing::{debug, info};
 
-use crate::cli::devnet::DevnetCli;
+use crate::{cli::devnet::DevnetCli, simulations::token_prices_update_new_pools};
 
 pub(crate) async fn run_devnet(executor: TaskExecutor, cli: DevnetCli) -> eyre::Result<()> {
+    token_prices_update_new_pools::run_devnet(executor, cli).await?;
+
+    Ok(())
+}
+
+async fn basic_example(executor: TaskExecutor, cli: DevnetCli) -> eyre::Result<()> {
     let config = cli.make_config()?;
     let mut testnet =
         AngstromTestnet::spawn_devnet(NoopProvider::default(), config, executor.clone())
