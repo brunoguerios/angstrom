5294aacda4aaca65579778cff5eb676d5f1e50fb
Will Smith <williamqsmith11@gmail.com>
2025-05-13 13:31:50 -0400

    wip

--- crates/rpc/src/types/mod.rs
+++ crates/rpc/src/types/mod.rs
@@ -1,9 +1,10 @@
 pub mod quoting;
 pub mod subscriptions;
 use alloy_primitives::FixedBytes;
-use angstrom_types::sol_bindings::grouped_orders::AllOrders;
+use angstrom_types::{primitive::OrderValidationError, sol_bindings::grouped_orders::AllOrders};
 pub use quoting::*;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 pub use subscriptions::*;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Hash)]
@@ -11,4 +12,26 @@ pub struct PendingOrder {
     /// the order id
     pub order_id: FixedBytes<32>,
     pub order:    AllOrders
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Hash)]
+pub struct CallResult {
+    is_success: bool,
+    data:       Value,
+    /// only will show up on error
+    msg:        String
+}
+
+impl From<OrderValidationError> for CallResult {
+    fn from(value: OrderValidationError) -> Self {
+        let msg = value.to_string();
+
+        let data = if let OrderValidationError::StateError(state) = &value {
+            serde_json::to_value(state).unwrap()
+        } else {
+            serde_json::to_value(value).unwrap()
+        };
+
+        Self { is_success: false, data, msg }
+    }
 }
