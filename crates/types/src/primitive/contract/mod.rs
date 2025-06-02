8576378da87a777d181a6ebd7d9e50b3a90d592f
Will Smith <williamqsmith11@gmail.com>
2025-06-02 12:59:19 -0500

    update angstrom flags

--- crates/types/src/primitive/contract/mod.rs
+++ crates/types/src/primitive/contract/mod.rs
@@ -86,7 +86,7 @@ pub struct AngstromAddressConfig {
 
 impl AngstromAddressConfig {
     pub const INTERNAL_TESTNET: Self = Self {
-        angstrom_address:         address!("0x293954613283cC7B82BfE9676D3cc0fb0A58fAa0"),
+        angstrom_address:         address!("0xc856DdFC924E9AeEaaFfB1905544b36470AC3ad4"),
         position_manager_address: address!("0xF967Ede45ED04ec89EcA04a4c7175b6E0106e3A8"),
         controller_v1_address:    address!("0xEd421745765bc1938848cAaB502ffF53c653ff13"),
         pool_manager_address:     address!("0x48bC5A530873DcF0b890aD50120e7ee5283E0112"),
