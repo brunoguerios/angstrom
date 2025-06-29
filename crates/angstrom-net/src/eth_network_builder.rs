47568e75a73da9abdaf2669167b40d859b7ed144
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-06-29 17:02:03 +0100

    bumped

--- crates/angstrom-net/src/eth_network_builder.rs
+++ crates/angstrom-net/src/eth_network_builder.rs
@@ -1,14 +1,13 @@
 use std::time::Duration;
 
 use reth::{chainspec::ChainSpec, transaction_pool::TransactionPool};
-use reth_network::{
-    EthNetworkPrimitives, NetworkHandle, NetworkManager, PeersInfo, protocol::IntoRlpxSubProtocol
-};
+use reth_eth_wire::BasicNetworkPrimitives;
+use reth_network::{NetworkHandle, NetworkManager, PeersInfo, protocol::IntoRlpxSubProtocol};
 use reth_node_builder::{
-    BuilderContext, NodeTypes, TxTy, components::NetworkBuilder, node::FullNodeTypes
+    BuilderContext, NodeTypes, PrimitivesTy, TxTy, components::NetworkBuilder, node::FullNodeTypes
 };
-use reth_primitives::{EthPrimitives, PooledTransaction};
-use reth_transaction_pool::PoolTransaction;
+use reth_primitives::EthPrimitives;
+use reth_transaction_pool::{PoolPooledTx, PoolTransaction};
 
 /// A basic ethereum payload service.
 pub struct AngstromNetworkBuilder<I: IntoRlpxSubProtocol + Send> {
@@ -25,18 +24,18 @@ impl<Node, Pool, I> NetworkBuilder<Node, Pool> for AngstromNetworkBuilder<I>
 where
     I: IntoRlpxSubProtocol + Send,
     Node: FullNodeTypes<Types: NodeTypes<ChainSpec = ChainSpec, Primitives = EthPrimitives>>,
-    Pool: TransactionPool<
-            Transaction: PoolTransaction<Consensus = TxTy<Node::Types>, Pooled = PooledTransaction>
-        > + Unpin
+    Pool: TransactionPool<Transaction: PoolTransaction<Consensus = TxTy<Node::Types>>>
+        + Unpin
         + 'static
 {
-    type Network = NetworkHandle<EthNetworkPrimitives>;
+    type Network =
+        NetworkHandle<BasicNetworkPrimitives<PrimitivesTy<Node::Types>, PoolPooledTx<Pool>>>;
 
     async fn build_network(
         self,
         ctx: &BuilderContext<Node>,
         pool: Pool
-    ) -> eyre::Result<NetworkHandle> {
+    ) -> eyre::Result<Self::Network> {
         let mut network_config = ctx.network_config()?;
         network_config.extra_protocols.push(self.custom_protocol);
         // try to dial outbound 5x quicker
