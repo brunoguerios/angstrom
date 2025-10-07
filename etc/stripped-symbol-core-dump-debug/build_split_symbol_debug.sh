10fc36e1551a2a26b0baefb0e1776177c82e2721
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-10-07 13:10:38 -0400

    as work

---
+++ etc/stripped-symbol-core-dump-debug/build_split_symbol_debug.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+ANG_DIR=/home/shared/angstrom
+PROD_BIN=/root/prod-bin-debug
+
+RUSTFLAGS="-C target-cpu=native -C force-frame-pointers=yes" cargo build --bin angstrom --profile maxperf-ss-debug --bin angstrom --features jemalloc --manifest-path ${ANG_DIR}/Cargo.toml -j 11
+
+
+BIN=${PROD_BIN}/angstrom-new
+DBG=${BIN}.debug
+
+cp ${BIN} ${PROD_BIN}/angstrom-old
+cp ${ANG_DIR}/target/maxperf-ss-debug/angstrom-new ${BIN}
+
+# Extract symbols to a separate file
+objcopy --only-keep-debug "$BIN" "$DBG"
+
+# Strip debug from the binary you deploy (fastest runtime, smallest RSS/I-cache)
+strip --strip-debug --preserve-dates "$BIN"
+
+# Add a link in the binary that points to the external debug file
+objcopy --add-gnu-debuglink="$DBG" "$BIN"
+
+
+# Install the .debug by Build-ID so coredumpctl/gdb can auto-find it
+buildid=$(readelf -n "$BIN" | awk '/Build ID/ {print $3; exit}')
+prefix=${buildid:0:2}; rest=${buildid:2}
+install -D "$DBG" "${INSTALL_DEBUG_DIR}/${prefix}/${rest}.debug"
+
+
+ulimit -c unlimited
+
+
+coredumpctl gdb ${BIN} \
+  --batch -ex "set pagination off" \
+  -ex "thread apply all bt full" \
+  -ex "info registers" \
+  -ex "quit" | sed -n '1,200p'
