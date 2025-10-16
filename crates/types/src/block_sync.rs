3bd54829ee14a920fe435d1492df67c391904573
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-10-16 13:33:47 -0400

    forge fmt

--- crates/types/src/block_sync.rs
+++ crates/types/src/block_sync.rs
@@ -130,7 +130,7 @@ impl BlockSyncProducer for GlobalBlockSync {
     }
 
     fn reorg(&self, reorg_range: RangeInclusive<u64>) {
-        let max_reorg_block = reorg_range.as_ref().max().unwrap();
+        let max_reorg_block = *reorg_range.iter().max().unwrap();
         while self.is_transitioning(max_reorg_block) {
             std::hint::spin_loop();
         }
