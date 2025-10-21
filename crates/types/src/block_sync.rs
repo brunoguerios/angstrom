322662877615a3a05da58d084adbdd6dcabdeb4d
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-10-21 13:21:17 -0400

    as updated

--- crates/types/src/block_sync.rs
+++ crates/types/src/block_sync.rs
@@ -108,8 +108,8 @@ impl BlockSyncProducer for GlobalBlockSync {
                         false
                     }
                 })
-                .unwrap_or(true)
-        })
+                .unwrap_or_default()
+        }) && self.all_modules_registered.load(Ordering::Relaxed)
     }
 
     fn new_block(&self, block_number: u64) {
